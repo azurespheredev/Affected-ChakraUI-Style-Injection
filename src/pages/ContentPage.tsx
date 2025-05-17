542af71d605daec392d08efc451d46310cc0ac1a
azurespheredev <24680042+azurespheredev@users.noreply.github.com>
2025-05-17 12:57:56 -0400

    fix: boilerplate issues

--- src/pages/ContentPage.tsx
+++ src/pages/ContentPage.tsx
@@ -3,7 +3,8 @@ import { Button, Flex, Text } from "@chakra-ui/react";
 export default function ContentPage() {
   return (
     <Flex direction={"column"} justify={"center"} align={"center"} gap={4} p={4}>
-      <Text>{"Example Content Page"}</Text>
+      <Text fontSize={"2xl"}>{"Example Title"}</Text>
+      <Text>{"Example Page Description"}</Text>
 
       <Button w={"full"}>{"Click Me"}</Button>
     </Flex>
